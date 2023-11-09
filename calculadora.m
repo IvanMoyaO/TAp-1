47ee176597ca752dbde13448034e8555830c4d21
IvanMoyaO <90717492+IvanMoyaO@users.noreply.github.com>
2023-11-09 13:47:53 +0100

    Arreglar cálculo mwevap, ma

--- calculadora.m
+++ calculadora.m
@@ -1,17 +1,17 @@
 classdef calculadora
-    %CALCULADORA Summary of this class goes here
-    %   Detailed explanation goes here
     
     properties
         mva = 0.622; %Masa vapor agua
         a = 16.54; %Antoine
         b = 3985; %Antoine
         c = -39.0; %Antoine
-        cpa = 1004;
-        cpv = 2050;
+        cpa = 1005; %cp aire
+        cpv = 1820; %cp agua
         pamb; %presion ambiente
         Tamb; %temperatura ambiente
-        cagua = 4180;
+        cagua = 4180; %c agua
+        T0 = 273; %temperatura de referencia
+        h0lv = 2501700; %entalpia de vaporizaion del agua
     end
     
     methods
@@ -22,6 +22,8 @@ classdef calculadora
         end
         
      function p_sat = psat(obj, varargin)
+         %funcion sobrecargada, obtiene Psat a partir de la Temperatura.
+         %si no se especifica, usa la ambiente (definida en el constructor)
             if nargin==1
                 T = obj.Tamb;%psat()
             elseif nargin==2
@@ -33,6 +35,10 @@ classdef calculadora
         end
 
      function omega = omega(obj, varargin)
+         %funcion sobrecargada, obtiene la humedad absoluta a partir de la
+         %presion de saturacion, la HR y la presion. 
+         %si la presion no se especifica, usa la ambiente (def en el
+         %constructor)
             if nargin==3 %omega(ps, phi)
                 p = obj.pamb;
             elseif nargin==4 %omega(ps, phi, p)
@@ -47,6 +53,9 @@ classdef calculadora
      end
 
      function tdew = tdew(obj, varargin)
+         %funcion sobrecargada, que obtiene la temperatura de rocio a
+         %partir de la HR y la temperatura. Si la temperatura no se
+         %especifica, usa la ambiente (definda en el constructir)
             if nargin==2
                 T = obj.Tamb;%tdew(phi)
             elseif nargin==3
@@ -60,15 +69,19 @@ classdef calculadora
      end
 
      function ma = ma(obj, T1a, T2a, T1w, T2w, omega1, omega2, mw1)
-         T0 = 273;
-         h0lv = 2257;
-         ma = ( (omega2 - omega1)*h0lv + obj.cpv * (omega2 * (T2a - T0) ...
-             - omega1 * (T1a - T0)) + (mw1 * obj.cagua * (T2w - T0)) - mw1 ...
-             * obj.cagua * (T1w - T0))/( obj.cpa * (T1a - T2a) - ...
-             (omega1 - omega2));
+         %obtiene el gasto masico a partir de las temperaturas de entrada y
+         %salida del agua y del aire, de las humedas absolutas y el gasto
+         %masico del agua.
+         h1 = obj.cpa * (T1a - obj.T0) + omega1 * (obj.h0lv + obj.cpv * (T1a - obj.T0));
+         h2 = obj.cpa * (T2a - obj.T0) + omega2 * (obj.h0lv + obj.cpv * (T2a - obj.T0));
+         h3 = obj.cagua*(T1w - obj.T0);
+         h4 = obj.cagua*(T2w - obj.T0);
+         ma = (mw1 * (h4 - h3))/(h1 + (omega2-omega1)*h4 - h2);
      end
     
      function mwevap = mwevap(~, omega1, omega2, ma)
+         %obtiene la masa de agua evaporada (que coincidira con la
+         %aportada) a partir de las humedades absolutas y el gasto masico.
          mwevap = ma*(omega2 - omega1);
      end
    end
